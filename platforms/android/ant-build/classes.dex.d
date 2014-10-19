dcfceabf912d011dca962c57bf2345550bad72b6
pula2000 <pulastya2000@gmail.com>
2014-10-19 14:14:42 -0500

    Pushed the entire code

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\hello\platforms\android\ant-build\classes.dex : \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\BuildConfig.class \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\CordovaApp.class \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\R$attr.class \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\R$drawable.class \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\R$string.class \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\R$xml.class \
+C:\hello\platforms\android\ant-build\classes\com\example\hello\R.class \
+C:\hello\platforms\android\ant-build\dexedLibs\classes-9c48fd5c1500e3de498e0db4e7385d74.jar \
